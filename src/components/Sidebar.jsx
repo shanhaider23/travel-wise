9fce40a873dce623fee024912f199298dc85a8a5
shanhaider23 <shan.haider@gmail.com>
2024-11-07 08:58:29 +0100

    added new updates

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,24 @@
-import { Outlet } from "react-router-dom";
-import AppNav from "./AppNav";
-import Logo from "./Logo";
-import styles from "./Sidebar.module.css";
+import { Outlet } from 'react-router-dom';
+import AppNav from './AppNav';
+import Logo from './Logo';
+import styles from './Sidebar.module.css';
 
 function Sidebar() {
-  return (
-    <div className={styles.sidebar}>
-      <Logo />
-      <AppNav />
+	return (
+		<div className={styles.sidebar}>
+			<Logo />
+			<AppNav />
 
-      <Outlet />
+			<Outlet />
 
-      <footer className={styles.footer}>
-        <p className={styles.copyright}>
-          &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
-        </p>
-      </footer>
-    </div>
-  );
+			<footer className={styles.footer}>
+				<p className={styles.copyright}>
+					&copy; Developed by {new Date().getFullYear()} by Shan-e-Haider
+					Bukhari.
+				</p>
+			</footer>
+		</div>
+	);
 }
 
 export default Sidebar;
